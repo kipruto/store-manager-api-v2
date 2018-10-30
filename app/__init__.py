6bec2074350984cc4a7c23cd9d2c383407c631ca
Kipruto Barno <barxwells@gmail.com>
2018-10-30 20:16:56 +0300

    [Finishes #161580082] refactoring database management module and commented on the create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,13 @@ from instance.config import app_config
 
 
 def create_app(config_name):
+    """
+    This function creates the flask application. Doing it this way
+    allows me to pass in different configuration settings from the
+    config file
+    :param config_name:
+    :return: app
+    """
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     return app
