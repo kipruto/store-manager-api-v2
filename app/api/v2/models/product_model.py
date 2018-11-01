126c09bd064e2582c17835c8f9724ff9cd735961
Kipruto Barno <barxwells@gmail.com>
2018-11-01 09:29:39 +0300

    [Feature 161177217] created specific product functions and views

--- app/api/v2/models/product_model.py
+++ app/api/v2/models/product_model.py
@@ -39,3 +39,22 @@ class Product:
             )
             rows.append(datum)
         return rows
+
+    def get_specific_product(self, product_id):
+        cursor = self.db.cursor()
+        cursor.execute("""SELECT product_id, category_id, product_name, unit_price, 
+        inventory_level, minimum_inventory_level FROM products WHERE product_id={} """.format(product_id))
+        data = cursor.fetchall()
+        row = []
+        for i, items in enumerate(data):
+            product_id, category_id, product_name, unit_price, inventory_level, minimum_inventory_level = items
+            datum = dict(
+                product_id=int(product_id),
+                category_id=int(category_id),
+                product_name=product_name,
+                unit_price=unit_price,
+                inventory_level=int(inventory_level),
+                minimum_inventory_level=int(minimum_inventory_level)
+            )
+            row.append(datum)
+        return row
